d811fa3da539027d3e6b2bb5d68f6dc148e98405
HuynhQuiLe <lehuynhqui1996@gmail.com>
2023-10-31 17:10:27 +0700

    update

--- src/service/configService.js
+++ src/service/configService.js
@@ -1,23 +1,25 @@
-export const USER_BASE_URL = "http://localhost:6868/users";
+// const BASE_URL = "http://localhost:6868/";
+const BASE_URL = "https://unknown-movie.adaptable.app/";
 
-export const CAROUSEL_BASE_URL = "http://localhost:6868/carousels";
+export const USER_BASE_URL = `${BASE_URL}users`;
 
-export const MOVIES_BASE_URL = " http://localhost:6868/movies";
+export const CAROUSEL_BASE_URL = `${BASE_URL}carousels`;
 
-export const COMMENT_BASE_URL = " http://localhost:6868/comments";
+export const MOVIES_BASE_URL = `${BASE_URL}movies`;
 
-export const THEATER_BASE_URL = "http://localhost:6868/theaters";
+export const COMMENT_BASE_URL = `${BASE_URL}comments`;
 
-export const BRANCH_BASE_URL = "http://localhost:6868/branches";
+export const THEATER_BASE_URL = `${BASE_URL}theaters`;
 
-export const SCHEDULE_IN_TOTAL_BASE_URL =
-  "http://localhost:6868/schedulesInTotal";
+export const BRANCH_BASE_URL = `${BASE_URL}branches`;
 
-export const SEAT_BASE_URL = "http://localhost:6868/seats";
+export const SCHEDULE_IN_TOTAL_BASE_URL = `${BASE_URL}schedulesInTotal`;
 
-export const TINH_ANH_BASE_URL = "http://localhost:6868/users/tinh-anh";
+export const SEAT_BASE_URL = `${BASE_URL}seats`;
 
-export const TICKET_BASE_URL = "http://localhost:6868/tickets";
+export const TINH_ANH_BASE_URL = `${BASE_URL}users/tinh-anh`;
+
+export const TICKET_BASE_URL = `${BASE_URL}tickets`;
 
 export const THEATER_BASE_URL1 =
   "https://movienew.cybersoft.edu.vn/api/QuanLyRap";
